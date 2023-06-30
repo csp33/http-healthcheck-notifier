620d0dc62c213f1cff688beecdf6e0f526783928
Carlos Sánchez Páez <sanchezpaezcarlos33@gmail.com>
2023-07-01 00:18:38 +0200

    Replace prints by logger

--- healthcheck.py
+++ healthcheck.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 import os
 
 import requests
@@ -7,6 +8,23 @@ import telegram
 telegram_token = os.environ["TELEGRAM_TOKEN"]
 telegram_chat_id = os.environ["TELEGRAM_DESTINATION_CHAT_ID"]
 endpoint_url = os.environ["URL_TO_CHECK"]
+
+
+def get_logger(log_level=logging.INFO):
+    log = logging.getLogger()
+    log.setLevel(log_level)
+    formatter = logging.Formatter(
+        fmt="[%(asctime)s][%(lineno)d][%(pathname)s][%(levelname)s]: %(message)s",
+        datefmt="%m/%d/%Y,%H:%M:%S",
+    )
+    handler = logging.StreamHandler()
+    handler.setLevel(log_level)
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
+    return log
+
+
+logger = get_logger()
 
 
 async def send_telegram_message(message):
@@ -18,9 +36,10 @@ async def check_remote_server():
     try:
         response = requests.get(endpoint_url, timeout=30)
         response.raise_for_status()
+        logger.info(f"Connection with {endpoint_url} successfully established")
     except requests.exceptions.RequestException:
         await send_telegram_message(f"Unable to connect to {endpoint_url}")
-        print("Server is not available. Telegram message sent.")
+        logger.warning("Server is not available. Telegram message sent.")
 
 
 if __name__ == "__main__":
